b3f37586faf1c7fdf0f50c0508be373a555eadd1
Laurent Savarit <laurent.savarit@laposte.net>
2021-08-18 11:16:58 +0200

    Revert "add github api connect in progress"
    This reverts commit 038639b1622d4477e56d17ea619ee04db57d5801.

--- components/contact.vue
+++ components/contact.vue
@@ -12,32 +12,56 @@
                 <!-- Contact Section Form-->
                 <div class="row justify-content-center">
                     <div class="col-lg-8 col-xl-7">
-                        <form id="contactForm" @submit="onSubmitFormContact">
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
                             <!-- Name input-->
                             <div class="form-floating mb-3">
-                                <input class="form-control" id="name" name="name" v-model="name" @input="onChange" type="text" placeholder="Entrer votre nom et prénom"/>
+                                <input class="form-control" id="name" type="text" placeholder="Entrer votre nom et prénom" data-sb-validations="required" />
                                 <label for="name">Nom et prénom</label>
-
+                                <div class="invalid-feedback" data-sb-feedback="name:required">Votre nom doit être saisi</div>
                             </div>
                             <!-- Email address input-->
                             <div class="form-floating mb-3">
-                                <input class="form-control" v-model="email" name="email" @input="onChange" id="email" type="email" placeholder="name@example.com"/>
-                                <label for="email">Votre adresse mail</label>
-
+                                <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                                <label for="email">Vote adresse mail</label>
+                                <div class="invalid-feedback" data-sb-feedback="email:required">L'adresse mail est obligatoire</div>
+                                <div class="invalid-feedback" data-sb-feedback="email:email">Voter email est invalide</div>
                             </div>
                             <!-- Phone number input-->
                             <div class="form-floating mb-3">
-                                <input class="form-control" v-model="phone" name="phone" @input="onChange" id="phone" type="tel" placeholder="(123) 456-7890"/>
+                                <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
                                 <label for="phone">Numéro de téléphone</label>
-
+                                <div class="invalid-feedback" data-sb-feedback="phone:required">Le numéro de téléphone est obligatoire</div>
                             </div>
                             <!-- Message input-->
                             <div class="form-floating mb-3">
-                                <textarea class="form-control" name="message" v-model="message" @input="onChange" id="message" type="text" placeholder="Votre message...." style="height: 10rem"></textarea>
+                                <textarea class="form-control" id="message" type="text" placeholder="Votre message...." style="height: 10rem" data-sb-validations="required"></textarea>
                                 <label for="message">Message</label>
-
+                                <div class="invalid-feedback" data-sb-feedback="message:required">Le message ne doit pas être vide</div>
                             </div>
-
+                            <!-- Submit success message-->
+                            <!---->
+                            <!-- This is what your users will see when the form-->
+                            <!-- has successfully submitted-->
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    To activate this form, sign up at
+                                    <br />
+                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>
+                            <!-- Submit error message-->
+                            <!---->
+                            <!-- This is what your users will see when there is-->
+                            <!-- an error submitting the form-->
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
                             <!-- Submit Button-->
                             <button class="btn btn-primary btn-xl" id="submitButton" type="submit">Envoyer</button>
                         </form>
@@ -49,67 +73,6 @@
 
 <script>
 export default {
-  name:"Contact",
-
-  data(){
-    return{
-      name:"",
-      email:"",
-      phone:"",
-      message:"",
-      valid:true
-    }
-  },
-
-  methods:{
-    onChange(event){
-
-      const input = event.target;
-
-      switch(input.name){
-
-        case "name":
-        case "phone":
-        case "message":
-          if(this[input.name] === ""){
-            input.style.border = "1px solid red";
-            this.valid = false;
-          }else{
-            input.style.border = "none";
-            this.valid = true;
-          }
-        break;
-
-        case "email":
-          const regexpEmail = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-          const checkEmail = regexpEmail.test(this[input.name]);
-
-          if(!checkEmail){
-            input.style.border = "1px solid red";
-            this.valid = false;
-          }else{
-            input.style.border = "none";
-            this.valid = true;
-          }
-
-        break;
-
-         default: return;
-      }
-    },
-
-    onSubmitFormContact(event){
-      event.preventDefault();
-
-      //TODO traiter l'envoi du mail au serveur
-      if(this.valid){
-      console.log(this.$data)
-      event.target.reset();
-      }else{
-        console.log("form invalide....")
-      }
-    }
-  }
-
+  name:"Contact"
 }
 </script>
