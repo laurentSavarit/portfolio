038639b1622d4477e56d17ea619ee04db57d5801
Laurent Savarit <laurent.savarit@laposte.net>
2021-08-18 11:14:46 +0200

    add github api connect in progress

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -12,15 +12,21 @@
                 </div>
                 <!-- Portfolio Grid Items-->
                 <div class="row justify-content-center">
+
+
+
                     <!-- Portfolio Item 1-->
-                    <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="col-md-6 col-lg-4 mb-5" v-for="repo in repos" :key="repo.id">
+                      <p style="color: black;">{{repo.name}}</p>
                         <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
                             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
                         </div>
                     </div>
+
+
+
                 </div>
             </div>
         </section>
@@ -28,6 +34,30 @@
 
 <script>
 export default {
-  name:"Portfolio"
+  name:"Portfolio",
+
+  data(){
+    return{
+      repos:[],
+    }
+
+  },
+
+  async mounted() {
+
+    const request = await fetch("https://api.github.com/user/repos?type=public",{
+      headers:{
+        "Accept":"application/vnd.github.v3+json",
+        "Authorization":"token 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"
+
+      }
+    })
+
+  const allRepos = await request.json();
+  this.$data.repos = [...allRepos];
+
+  console.log(this.$data.repos)
+
+  },
 }
 </script>
