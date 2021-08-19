f9ee32656861844e662056547044170c0405922d
Laurent Savarit <laurent.savarit@laposte.net>
2021-08-19 18:25:36 +0200

    deploy on heroku

--- test/footer.spec.js
+++ test/footer.spec.js
@@ -3,7 +3,11 @@ import footer from '@/components/footer.vue'
 
 describe('Footer', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(footer)
+    const wrapper = mount(footer,{
+      stubs:{
+        NuxtLink: true
+      }
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
--- test/nav.spec.js
+++ test/nav.spec.js
@@ -3,7 +3,11 @@ import nav from '@/components/nav.vue'
 
 describe('Journey', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(nav)
+    const wrapper = mount(nav,{
+      stubs:{
+        NuxtLink: true
+      }
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
